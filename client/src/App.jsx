25ae76d7be52e586333c6d60fccd8764f1a2d0df
thijsBoet <m.boet@chello.nl>
2023-01-23 14:25:10 +0100

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	return <h1 className='text-3xl text-gray-500'>Hello</h1>;
+};
+
+export default App;
