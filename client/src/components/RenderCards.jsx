e74aebf4691d02589e9b5515835e6ac352d55714
thijsBoet <m.boet@chello.nl>
2023-01-23 15:57:46 +0100

    finished UI frontend starting backend

---
+++ client/src/components/RenderCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RenderCards = ({ data, title }) => {
+	if (data?.length > 0) {
+		return data.map(post => <Card key={post._id} {...post} />);
+	}
+
+	return (
+		<h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+	);
+};
+
+export default RenderCards
